1856c3e8a96166bd86858fb5ca57367ef0a82755
buuhsmead <hdaems@redhat.com>
2023-04-06 15:34:10 +0200

    bootstrapping cluster-config

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+
+LANG=C
+SLEEP_SECONDS=5
+
+OVERLAY=default
+
+if [ $# -gt 0 ]; then
+    OVERLAY=$1
+    echo "Using specified overlay $1"
+fi
+
+if [ ! -d "openshift-gitops-operator/overlays/${OVERLAY}" ]; then
+    echo "Overlay ${OVERLAY} does not exist in path openshift-gitops-operator/overlays/${OVERLAY}"
+    exit 1
+fi
+
+echo ""
+echo "Installing GitOps Operator."
+
+kustomize build "openshift-gitops-operator/overlays/${OVERLAY}" | oc apply -f -
+
+echo "Pause $SLEEP_SECONDS seconds for the creation of the gitops-operator..."
+sleep $SLEEP_SECONDS
+
+echo "Waiting for operator to start"
+until oc get deployment gitops-operator-controller-manager -n openshift-operators
+do
+  sleep 5;
+done
+
+echo "Waiting for openshift-gitops namespace to be created"
+until oc get ns openshift-gitops
+do
+  sleep 5;
+done
+
+echo "Waiting for deployments to start"
+until oc get deployment cluster -n openshift-gitops
+do
+  sleep 5;
+done
+
+echo "Waiting for all pods to be created"
+deployments=(cluster kam openshift-gitops-applicationset-controller openshift-gitops-redis openshift-gitops-repo-server openshift-gitops-server)
+for i in "${deployments[@]}";
+do
+  echo "Waiting for deployment $i";
+  oc rollout status deployment $i -n openshift-gitops
+done
+
+echo "Apply overlay to override default instance"
+# echo "Create default instance of gitops operator"
+kustomize build openshift-gitops-instance/overlays/${OVERLAY} | oc apply -f -
+
+sleep 10
+echo "Waiting for all pods to redeploy"
+deployments=(cluster kam openshift-gitops-applicationset-controller openshift-gitops-redis openshift-gitops-repo-server openshift-gitops-server)
+for i in "${deployments[@]}";
+do
+  echo "Waiting for deployment $i";
+  oc rollout status deployment $i -n openshift-gitops
+done
+
+echo "GitOps Operator ready"
+
+
+echo "Apply cluster config application"
+kustomize build cluster-config/ | oc apply -f -
